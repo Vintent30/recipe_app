0612fa5fae0fdea52434df179afd0ff9e5a8e3ee
lethanhy <lethanhy220304@gmail.com>
2024-09-30 05:13:59 -0700

    capnhatlan1

--- app/src/main/java/com/example/recipe_app/MainActivity.java
+++ app/src/main/java/com/example/recipe_app/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.user);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.User), (v, insets) -> {
+        setContentView(R.layout.favourite);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.favourite), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
